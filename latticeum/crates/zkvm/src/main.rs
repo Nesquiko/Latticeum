6340e2f7289406a6b952f9f7c84e69ea59264bd0
Nesquiko <lukas.castven@gmail.com>
2025-09-14 13:26:08 +0200

    Arithmetize and commit to each step

--- latticeum/crates/zkvm/src/main.rs
+++ latticeum/crates/zkvm/src/main.rs
@@ -13,7 +13,11 @@ use latticefold::{
 use num_traits::identities::Zero;
 use std::{path::PathBuf, usize};
 
-use vm::riscvm::{inst::ExectionTrace, riscv_isa::Instruction, vm::new_vm};
+use vm::riscvm::{
+    inst::{ExectionTrace, MemoryOperation},
+    riscv_isa::Instruction,
+    vm::new_vm,
+};
 
 use crate::{ccs::CCSLayout, constraints::CCSBuilder};
 
@@ -21,14 +25,13 @@ use crate::{ccs::CCSLayout, constraints::CCSBuilder};
 pub struct GoldiLocksDP;
 
 impl DecompositionParams for GoldiLocksDP {
-    /// Half of word in 32 bit VM
-    const B: u128 = 1 << 16;
-    /// Ring modulus is GoldiLocks so little less than 2^64, thus GoldiLocks modulus < B^L (which is `2^(16 * 4)`)
-    const L: usize = 4;
+    const B: u128 = 1 << 32;
+    /// Ring modulus is GoldiLocks (little less than 2^64), thus GoldiLocks modulus < B^L
+    const L: usize = 2;
     /// Standard binary decomposition
     const B_SMALL: usize = 2;
-    /// Log(B) = Log(1 << 16) = Log(2 ^ 16) = 16
-    const K: usize = 16;
+    /// Log(B) (base 2 log)
+    const K: usize = 32;
 }
 
 const CCS_LAYOUT: CCSLayout = CCSLayout::new();
@@ -58,11 +61,62 @@ fn main() {
 
     let (mut acc, mut w_acc) = initialize_accumulator(&ccs, &CCS_LAYOUT, &scheme);
 
-    vm.run(|trace| trace_step(trace, &CCS_LAYOUT, &ccs));
+    vm.run(|trace| {
+        trace_step(trace, &CCS_LAYOUT, &ccs, &scheme);
+    });
 
     let expected_value = 0x34164a7b;
     println!("expected: 0x{:x}, got 0x{:x}", expected_value, vm.result());
     assert_eq!(expected_value, vm.result());
+}
+
+fn trace_step(
+    trace: &ExectionTrace,
+    layout: &CCSLayout,
+    ccs: &CCS<GoldilocksRingNTT>,
+    scheme: &AjtaiCommitmentScheme<C, W, GoldilocksRingNTT>,
+) {
+    arithmetize(trace, layout, ccs, scheme);
+}
+
+fn arithmetize(
+    trace: &ExectionTrace,
+    layout: &CCSLayout,
+    ccs: &CCS<GoldilocksRingNTT>,
+    scheme: &AjtaiCommitmentScheme<C, W, GoldilocksRingNTT>,
+) -> (CCCS<C, GoldilocksRingNTT>, Witness<GoldilocksRingNTT>) {
+    let raw_z = to_witness(trace, layout);
+    // convert raw z to proper z-vector structure: [x_ccs, 1, w_ccs]
+    let mut z_vec = Vec::new();
+    // x_ccs: public inputs (empty since ccs.l = 0)
+    // (no elements to add)
+
+    // Add constant 1
+    z_vec.push(1usize);
+
+    // w_ccs: witness elements
+    z_vec.extend(raw_z.iter().map(|&x| x as usize));
+
+    // 1. generate the full witness vector `z` as before
+    let z_as_ring: Vec<GoldilocksRingNTT> = to_F_vec(z_vec);
+
+    // 2. split the z vector into public IO (x_ccs) and private witness (w_ccs)
+    let x_ccs = z_as_ring[0..ccs.l].to_vec();
+    let w_ccs = z_as_ring[ccs.l + 1..].to_vec(); // +1 to skip the constant '1'
+
+    // 3. create the Witness struct
+    let wit = Witness::from_w_ccs::<GoldiLocksDP>(w_ccs);
+
+    // // 4. create the CCCS (Committed CCS) instance.
+    let cm = wit
+        .commit::<C, W, GoldiLocksDP>(scheme)
+        .expect("failed to commit");
+    let cccs_instance = CCCS { cm, x_ccs };
+
+    #[cfg(feature = "debug")]
+    check_relation_debug(&ccs, &z_as_ring, trace);
+
+    (cccs_instance, wit)
 }
 
 fn initialize_accumulator<const C: usize, const W: usize>(
@@ -71,8 +125,8 @@ fn initialize_accumulator<const C: usize, const W: usize>(
     scheme: &AjtaiCommitmentScheme<C, W, GoldilocksRingNTT>,
 ) -> (LCCCS<C, GoldilocksRingNTT>, Witness<GoldilocksRingNTT>) {
     // Create witness with private witness part only
-    // The z-vector structure is [x_ccs(0), 1, w_ccs(z_layout.size)] = z_layout.size + 1 total
-    // So the private witness (w_ccs) should be z_layout.size elements
+    // The z-vector structure is [x_ccs(0), 1, w_ccs(layout.size)] = layout.size + 1 total
+    // So the private witness (w_ccs) should be layout.size elements
     debug_assert_eq!(ccs.n - ccs.l - 1, layout.w_size);
     let zero_w_ccs = vec![GoldilocksRingNTT::zero(); layout.w_size];
 
@@ -94,32 +148,6 @@ fn initialize_accumulator<const C: usize, const W: usize>(
     .expect("failed to create initial accumulator");
 
     (acc, zero_wit)
-}
-
-fn trace_step(trace: &ExectionTrace, z_layout: &CCSLayout, ccs: &CCS<GoldilocksRingNTT>) {
-    arithmetize(trace, z_layout, ccs);
-}
-
-fn arithmetize(trace: &ExectionTrace, z_layout: &CCSLayout, ccs: &CCS<GoldilocksRingNTT>) {
-    let raw_z = to_witness(trace, z_layout);
-
-    // Convert raw witness to proper z-vector structure: [x_ccs, 1, w_ccs]
-    let mut z_vec = Vec::new();
-    // x_ccs: public inputs (empty since ccs.l = 0)
-    // (no elements to add)
-
-    // Add constant 1
-    z_vec.push(1usize);
-
-    // w_ccs: witness elements
-    z_vec.extend(raw_z.iter().map(|&x| x as usize));
-
-    let z: Vec<GoldilocksRingNTT> = to_F_vec(z_vec);
-
-    #[cfg(feature = "debug")]
-    {
-        check_relation_debug(&ccs, &z, trace);
-    }
 }
 
 #[cfg(feature = "debug")]
